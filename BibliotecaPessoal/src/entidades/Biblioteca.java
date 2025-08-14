38bf97369a93fbd1dfb4be2c956750b4c208287d
Marcos santos <marcosvn3@academico.ufs.br>
2025-08-14 16:21:05 -0300

    Refatorando e adicionando metodos!

--- BibliotecaPessoal/src/entidades/Biblioteca.java
+++ BibliotecaPessoal/src/entidades/Biblioteca.java
@@ -3,43 +3,46 @@ package entidades;
 import java.util.HashMap;
 
 public class Biblioteca {
-	private HashMap<String,Livro> estante;
-	
-	public Biblioteca () {
+	private HashMap<String, Livro> estante;
+
+	public Biblioteca() {
 		this.estante = new HashMap<>();
 	}
-	
+
 	public void adicionarLivroPorAutor(Livro livro) {
-		estante.put(livro.getAutor(),livro);
+		estante.put(livro.getAutor(), livro);
 		System.out.println("Livro adicionado com sucesso!");
 	}
-	
+
 	public void adicionarLivroPorTitulo(Livro livro) {
-		estante.put(livro.getTitulo(),livro);
+		estante.put(livro.getTitulo(), livro);
 		System.out.println("Livro adicionado com sucesso!");
 	}
-	
+
 	public void listarLivros() {
-		if(estante.isEmpty()) {
+		if (!estante.isEmpty()) {
+			estante.forEach((tituloL, livro) -> System.out.println("Livro" + tituloL + "-" + livro));
+		} else {
 			System.out.println("Estante vazia!");
-		}else {
-			estante.forEach((t,l) -> System.out.println("Livro: "+t+"-"+l));
 		}
+
 	}
-	
-	public void buscarLivroPorAutor(String autor) {	
-		if(!estante.containsKey(autor)) {
+
+	public void buscarLivroPorAutor(String autor) {
+		if (!estante.containsKey(autor)) {
 			System.out.println(autor + "- não encontrado");
-		}else {
+		} else {
 			estante.get(autor);
 		}
 	}
 	
+	public void buscarLivroPorTitulo(String titulo) {
+		if (!estante.containsKey(titulo)) {
+			System.out.println(titulo + "- não encontrado");
+		} else {
+			estante.get(titulo);
+		}
+	}
 	
-	
-	
-	
-	
-	
-	
+
 }
